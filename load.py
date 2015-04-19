8abb091aba9f2267abcdf8f0b83bfd3796603433
Todd Pollak <todd.pollak@tufts.edu>
2015-04-19 14:30:39 -0400

    adding python script

---
+++ load.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def main():
+    Url = "local"
+
+    for x in xrange(1, 25):
+        obj = requests.get("localhost:8080/api/1/hello")
+        print obj.text
+
+
+if __name__ == '__main__':
+        main()
