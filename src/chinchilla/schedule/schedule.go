d56cb1ed25e5d296e09a0e5a47eea3f1ea3b9abd
Todd Pollak <todd.pollak@tufts.edu>
2015-04-20 17:39:16 -0400

    round robin qVal

--- src/chinchilla/schedule/schedule.go
+++ src/chinchilla/schedule/schedule.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 )
 
-func RoundRobin(workers *types.MapQ) uint32 {
+func RoundRobin(workers *types.MapQ, typ uint8) uint32 {
 	for k, v := range workers.M {
+		fmt.Printf("node %d has a QVal of %f\n", k, v.QVal)
 		workers.L.Lock()
 		if !workers.M[k].Sent {
 			v.Sent = true
 			workers.M[k] = v
 			workers.L.Unlock()
+			UpQVal(workers, typ, k)
 			return k
 		}
 		workers.L.Unlock()
@@ -23,14 +25,19 @@ func RoundRobin(workers *types.MapQ) uint32 {
 		workers.L.Unlock()
 	}
 	for k, v := range workers.M {
+		workers.L.Lock()
 		v.Sent = true
 		workers.M[k] = v
+		workers.L.Unlock()
+		UpQVal(workers, typ, k)
+
 		return k
 	}
 	return 0
 }
 
 func ShortestQ(workers *types.MapQ, typ uint8) uint32 {
+	fmt.Println("in shortest Q")
 	first := true
 	var node uint32
 	var min float64
@@ -45,13 +52,20 @@ func ShortestQ(workers *types.MapQ, typ uint8) uint32 {
 				node = k
 			}
 		}
-		fmt.Printf("Node %d has a QVal of %f\n", k, v.QVal)
+		fmt.Printf("node %d has a QVal of %f\n", k, v.QVal)
 	}
+	for k, v := range workers.M[node].AvgTimes {
+		fmt.Printf("type %d has qval of %f\n", k, v)
+	}
+	UpQVal(workers, typ, node)
+	fmt.Printf("chose node %d\n", node)
+	return node
+}
+
+func UpQVal(workers *types.MapQ, typ uint8, node uint32) {
 	workers.L.Lock()
 	tmp := workers.M[node]
-	tmp.QVal += workers.M[node].AvgTimes[typ] + .01
+	tmp.QVal += workers.M[node].AvgTimes[typ] + .1
 	workers.M[node] = tmp
 	workers.L.Unlock()
-	fmt.Printf("Chose node %d for type %d\n", node, typ)
-	return node
 }
